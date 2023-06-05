29ed2fe7b58b6523af348b4817cd7dc3cd4e1bfd
nkeskin <keskin.nmn@gmail.com>
2023-06-05 19:16:20 +0300

    add pom file and a sample junit5 test

---
+++ demo/test/DemoTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+
+  @Test
+  void demoTest() {
+    Assertions.assertNotEquals(1, 0);
+  }
+
+}
