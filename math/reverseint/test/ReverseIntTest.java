4770f97dbd45e26213b62b90dec7884b9c45b7b3
nkeskin <keskin.nmn@gmail.com>
2023-06-13 22:34:08 +0300

    add implementation for reversing an int value

---
+++ math/reverseint/test/ReverseIntTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ReverseIntTest {
+
+  ReverseInt reverseInt;
+
+  @BeforeEach
+  void setUp() {
+    reverseInt = new ReverseInt();
+  }
+
+  @Test
+  void case1() {
+    Assertions.assertEquals(0, reverseInt.reverse(1534236469));
+  }
+
+  @Test
+  void case2() {
+    Assertions.assertEquals(123, reverseInt.reverse(321));
+  }
+
+  @Test
+  void case3() {
+    Assertions.assertEquals(-2143847412, reverseInt.reverse(-2147483412));
+  }
+
+
+
+}
