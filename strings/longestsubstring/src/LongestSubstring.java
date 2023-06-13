f51177414dd8b1a8bedbc0c8fe612b6ba3f82ab6
nkeskin <keskin.nmn@gmail.com>
2023-06-13 19:10:00 +0300

    add implementation for longest non-repeating substring for a given string input

--- strings/longestsubstring/src/LongestSubstring.java
+++ strings/longestsubstring/src/LongestSubstring.java
@@ -5,15 +5,13 @@ import java.util.Set;
 public class LongestSubstring {
 
   public int findLongest(String input) {
-    Set<Character> characterSet;
-    char[] chars = input.toCharArray();
+    Set<Character> characterSet = new HashSet<>();
     int max = 0;
     for (int i = 0; i < input.length() - 1; i++) {
-      characterSet = new HashSet<>();
-      characterSet.add(chars[i]);
+      characterSet.add(input.charAt(i));
       int temp = 1;
       for (int j = i + 1; j < input.length(); j++) {
-        if (characterSet.add(chars[j])) {
+        if (characterSet.add(input.charAt(j))) {
           temp++;
         } else {
           break;
@@ -22,6 +20,7 @@ public class LongestSubstring {
       if (temp > max) {
         max = temp;
       }
+      characterSet.clear();
     }
     return max;
   }
